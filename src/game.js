c4413c4636bf52c47c6e49787294e86b8c0c51e9
Nick <ndenbleyker@gmail.com>
2024-06-20 10:52:56 -0400

    added bricks and ball paddle collision logic

--- src/game.js
+++ src/game.js
@@ -1,6 +1,7 @@
 import Paddle from './paddle.js'
 import InputHandler from './input.js'
 import Ball from './ball.js'
+import Brick from './brick.js'
 
 export default class Game {
     constructor(gameWidth, gameHeight) {
@@ -14,19 +15,25 @@ export default class Game {
     start() {
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
+
+        let bricks = [];
+        for(let i=0; i<10; i++){
+            bricks.push(new Brick(this, {x: i* 52, y: 30}));
+        }
+        this.gameObjects = [
+            this.ball,
+            this.paddle,
+            ...bricks
+        ]
         new InputHandler(this.paddle);
     }
 
     update(deltaTime) {
-        this.paddle.update(deltaTime);
-        this.ball.update(deltaTime);
-
-
+        this.gameObjects.forEach((object) => object.update(deltaTime));
     }
 
     draw(ctx) {
-        this.paddle.draw(ctx);
-        this.ball.draw(ctx);
+        this.gameObjects.forEach(object => object.draw(ctx));
     }
 }
 
